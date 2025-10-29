93a48869ce37ad7390f036997c6d08c5f6a9ba5d
Hubert Piwowarski <01150042@pw.edu.pl>
2025-10-29 12:43:03 +0100

    zad 1 i 2 commit

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,31 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() // konstruktor domyslny
+    {
+        cos = new Resource();
+    }
+
+    double get() // metoda double get
+    {
+        return cos->get();
+    }
+
+    ResourceManager(const ResourceManager& i) // konstruktor kopiujacy
+    {
+        cos  = new Resource();
+        *cos = *(i.cos); // kopiuje wartosc
+    }
+
+    ResourceManager& operator=(const ResourceManager& i) // kopiujacy operator przypisania
+    {
+        *cos = *i.cos;
+        return *this; // gdy korzystamy z operatora przypisania korzystamy z return
+    }
+
+    ~ResourceManager() { delete cos; }
+
+private:
+    Resource* cos;
 };
